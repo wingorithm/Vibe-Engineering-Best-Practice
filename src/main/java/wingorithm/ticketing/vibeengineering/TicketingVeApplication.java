6037dca58a83f38a294c98c701978771367c651f
wingorithm <erwin.gunawan@binus.ac.id>
2026-04-25 00:39:36 +0700

    initial bootstrap

---
+++ src/main/java/wingorithm/ticketing/vibeengineering/TicketingVeApplication.java
@@ -0,0 +1,13 @@
+package wingorithm.ticketing.vibeengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingVeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingVeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wingorithm/ticketing/vibeengineering/TicketingVeApplicationTests.java
@@ -0,0 +1,13 @@
+package wingorithm.ticketing.vibeengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingVeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
